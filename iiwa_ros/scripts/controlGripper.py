439760e8d82277facd3d412b7160e98915b15e7b
Lasitha <lasitha@gatech.edu>
2019-04-28 18:42:39 -0400

    Working script to move arms to desired xyz location, disregards orientation

---
+++ iiwa_ros/scripts/controlGripper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import rospy
+import math
+import numpy
+from std_msgs.msg import Float64
+
+
+def gripObject():
+    rospy.init_node('gripperEngager')
+    message = Float64()
+    gripper1Publishers = [rospy.Publisher('/iiwa/EffortJointInterface_claw_1_joint_controller/command',
+                                          Float64), rospy.Publisher('/iiwa/EffortJointInterface_claw_2_joint_controller/command', Float64)]
+    gripper2Publishers = [rospy.Publisher('/iiwa/EffortJointInterface_2_claw_1_joint_controller/command',
+                                          Float64), rospy.Publisher('/iiwa/EffortJointInterface_2_claw_2_joint_controller/command', Float64)]
+    while not rospy.is_shutdown():
+        command = raw_input('Engage or Disengage gripper')
+        if (command == 'Engage gripper'):
+                torque = raw_input('Specify torque\n')
+                message.data = float(torque)
+                for publisher in gripper1Publishers:
+                    publisher.publish(message.data)
+                for publisher in gripper2Publishers:
+                    publisher.publish(message.data)
+        elif (command == 'Disengage gripper'):
+            message = Float64()
+            message.data = -0.01
+            for publisher in gripper1Publishers:
+                publisher.publish(message.data)
+            for publisher in gripper2Publishers:
+                publisher.publish(message.data)
+
+if __name__ == '__main__':
+    gripObject()
---
+++ iiwa_ros/scripts/gripBox.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python
+import rospy
+import math
+import numpy
+from std_msgs.msg import Float64
+from trac_ik_python.trac_ik import IK
+
+
+def gripObject():
+    rospy.init_node('ManipulatorCommandPublisher')
+    arm1Publishers = [rospy.Publisher('/iiwa/EffortJointInterface_J_1_controller/command', Float64), rospy.Publisher('/iiwa/EffortJointInterface_J_2_controller/command', Float64),
+                      rospy.Publisher('/iiwa/EffortJointInterface_J_3_controller/command', Float64), rospy.Publisher('/iiwa/EffortJointInterface_J_4_controller/command', Float64), rospy.Publisher('/iiwa/EffortJointInterface_gripper_base_joint_controller/command', Float64)]
+    arm2Publishers = [rospy.Publisher('/iiwa/EffortJointInterface_J_21_controller/command', Float64), rospy.Publisher('/iiwa/EffortJointInterface_J_22_controller/command', Float64),
+                      rospy.Publisher('/iiwa/EffortJointInterface_J_23_controller/command', Float64), rospy.Publisher('/iiwa/EffortJointInterface_J_24_controller/command', Float64), rospy.Publisher('/iiwa/EffortJointInterface_2_gripper_base_joint_controller/command', Float64)]
+    surfacePublisher = rospy.Publisher(
+        '/iiwa/EffortJointInterface_PS_controller/command', Float64)
+    gripper1Publishers = [rospy.Publisher('/iiwa/EffortJointInterface_claw_1_joint_controller/command',
+                                          Float64), rospy.Publisher('/iiwa/EffortJointInterface_claw_2_joint_controller/command', Float64)]
+    gripper2Publishers = [rospy.Publisher('/iiwa/EffortJointInterface_2_claw_1_joint_controller/command',
+                                          Float64), rospy.Publisher('/iiwa/EffortJointInterface_2_claw_2_joint_controller/command', Float64)]
+    ik_solver = IK("base_link", "gripper_base_link")
+    ik_solver2 = IK("base_link", "2_gripper_base_link")
+
+    print("IK solver uses link chain:")
+    print(ik_solver.link_names)
+
+    print("IK solver 2 uses link chain:")
+    print(ik_solver2.link_names)
+
+    print("IK solver base frame:")
+    print(ik_solver.base_link)
+
+    print("IK solver 2 base frame:")
+    print(ik_solver2.base_link)
+
+    print("IK solver tip link:")
+    print(ik_solver.tip_link)
+
+    print("IK solver 2 tip link:")
+    print(ik_solver2.tip_link)
+
+    print("IK solver for joints:")
+    print(ik_solver.joint_names)
+
+    print("IK solver 2 for joints:")
+    print(ik_solver2.joint_names)
+
+    lb, up = ik_solver.get_joint_limits()
+    print("Lower bound: " + str(lb))
+    print("Upper bound: " + str(up))
+
+    lb, up = ik_solver2.get_joint_limits()
+    print("Lower bound2: " + str(lb))
+    print("Upper bound2: " + str(up))
+
+    qinit = [0] * ik_solver.number_of_joints
+    qinit2 = [0] * ik_solver2.number_of_joints
+
+    brx = bry = brz = 9999
+    bx = by = bz = 0.001
+
+    while not rospy.is_shutdown():
+        message = Float64()
+        command = raw_input('Specify arm')
+        if int(command) == 1:
+            command = raw_input('Specify location X\n')
+            xyz = [float(command)]
+            command = raw_input('Specify location Y\n')
+            xyz.append(float(command))
+            command = raw_input('Specify location Z\n')
+            xyz.append(float(command))
+            command = raw_input('Specify location Rx\n')
+            rxyz = [float(command)]
+            command = raw_input('Specify location Ry\n')
+            rxyz.append(float(command))
+            command = raw_input('Specify location Rz\n')
+            rxyz.append(float(command))
+            sol = ik_solver.get_ik(qinit, xyz[0], xyz[1], xyz[2], rxyz[0], rxyz[1], rxyz[2], 1,
+                                   bx, by, bz,
+                                   brx, bry, brz)
+            print(sol)
+            if sol is not None:
+                print "X, Y, Z: " + str((xyz[0], xyz[1], xyz[2]))
+                print "SOL: " + str(sol)
+                command = raw_input("Go to coordinates? y/n")
+                if command == "y":
+                    qinit = sol
+                mylist = numpy.linspace(1, 10, 10)
+                count = 0
+                tempsol = list(sol)
+                for i in tempsol:
+                    tempsol[count] = i / len(mylist)
+                    count = count + 1
+                rate = rospy.Rate(2)
+                for i in mylist:
+                    count = 0
+                    for c in tempsol:
+                        message.data = c * i
+                        arm1Publishers[count].publish(message)
+                        count = count + 1
+                    rate.sleep()
+        elif int(command) == 2:
+            command = raw_input('Specify location X\n')
+            xyz = [float(command)]
+            command = raw_input('Specify location Y\n')
+            xyz.append(float(command))
+            command = raw_input('Specify location Z\n')
+            xyz.append(float(command))
+            command = raw_input('Specify location Rx\n')
+            rxyz = [float(command)]
+            command = raw_input('Specify location Ry\n')
+            rxyz.append(float(command))
+            command = raw_input('Specify location Rz\n')
+            rxyz.append(float(command))
+            sol = ik_solver2.get_ik(qinit2, xyz[0], xyz[1], xyz[2], rxyz[0], rxyz[1], rxyz[2], 1,
+                                    bx, by, bz,
+                                    brx, bry, brz)
+            print(sol)
+            if sol is not None:
+                print "X, Y, Z: " + str((xyz[0], xyz[1], xyz[2]))
+                print "SOL: " + str(sol)
+                command = raw_input("Go to coordinates? y/n")
+                if command == "y":
+                    qinit2 = sol
+                    mylist = numpy.linspace(1, 10, 10)
+                    count = 0
+                    tempsol = list(sol)
+                    for i in tempsol:
+                        tempsol[count] = i / len(mylist)
+                        count = count + 1
+                    rate = rospy.Rate(2)
+                    for i in mylist:
+                        count = 0
+                        for c in tempsol:
+                            message.data = c * i
+                            arm2Publishers[count].publish(message)
+                            count = count + 1
+                        rate.sleep()
+    # elif int(command) == 2:
+    #	command = raw_input('Gripper number? 1/2')
+    #	torque = float(raw_input('Specify torque'))
+    #	if int(command) == 1:
+    #			for i in gripper1Publishers:
+    #				message.data = torque
+    #				i.publish(message)
+    #	elif int(command) == 2:
+    #		for i in gripper2Publishers:
+    #				message.data = torque
+    #				i.publish(message)
+
+    # 	if (command == 'Engage gripper'):
+    #		torque = raw_input('Specify torque\n')
+    # 		message.data = float(torque)
+    # 		for publisher in clawPublishers:
+    # 			publisher.publish(message.data)
+    # 	elif (command == 'Disengage gripper'):
+    # 		message = Float64()
+    # 		message.data = -0.01
+    # 		for publisher in clawPublishers:
+    # 			publisher.publish(message)
+    # 	elif (command == 'Oscillate surface'):
+    # 		userinput = raw_input('Specify amplitude, offset and duration in s\n')
+    # 		userinput = userinput.split()
+    # 		time = numpy.linspace(0,float(userinput[2]),10*float(userinput[2]))
+    # 		rate = rospy.Rate(10)
+    # 		for t in time:
+    # 			pos = float(userinput[1])+float(userinput[0])*math.sin(math.pi*t)
+    # 			message.data=pos
+    # 			surfacePublisher.publish(message)
+    # 			rate.sleep()
+    # 	elif (command == 'Step response surface'):
+    # 		position = raw_input('Input step response set point\n')
+    # 		message.data = float(position)
+    # 		surfacePublisher.publish(message)
+    # 	else:
+    # 		print("Command not recognized")
+
+
+if __name__ == '__main__':
+    gripObject()
