3fb34188e8e7d50d8202afba0b6091126a072ece
Juan Vallejo <jvallejo3@gatech.edu>
2019-10-17 17:16:24 -0400

    Working dynamixel_interface

--- iiwa_control/Control/dynamixel_interface.py
+++ iiwa_control/Control/dynamixel_interface.py
@@ -36,30 +36,47 @@
 #     def getch():
 #         return msvcrt.getch().decode()
 # else:
-#     import sys, tty, termios
-#     fd = sys.stdin.fileno()
-#     old_settings = termios.tcgetattr(fd)
-#     def getch():
-#         try:
-#             tty.setraw(sys.stdin.fileno())
-#             ch = sys.stdin.read(1)
-#         finally:
-#             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+# import sys, tty, termios
+# fd = sys.stdin.fileno()
+# old_settings = termios.tcgetattr(fd)
+#
+#
+# def getch():
+#     try:
+#         tty.setraw(sys.stdin.fileno())
+#         ch = sys.stdin.read(1)
+#     finally:
+#         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 #         return ch
 
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
 import numpy as np
 
 # Control table address
-ADDR_PRO_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
-ADDR_PRO_GOAL_POSITION      = 116
-ADDR_PRO_PRESENT_POSITION   = 132
+ADDR_ID = 7
+ADDR_PRO_TORQUE_ENABLE = 64               # Control table address is different in Dynamixel model
+ADDR_PRO_GOAL_POSITION = 116              # CW is decreasing, CCW is increasing
+ADDR_PRO_PRESENT_POSITION = 132
+ADDR_MIN_POSITION_LIMIT = 52
+ADDR_MAX_POSITION_LIMIT = 48
+ADDR_OPERATING_MODE = 11
+ADDR_DRIVE_MODE = 10
+ADDR_HOMING_OFFSET = 20
+ADDR_POS_P_GAIN = 84
+ADDR_POS_D_GAIN = 80
+ADDR_POS_I_GAIN = 82
+ADDR_PROFILE_ACC = 108
+ADDR_PROFILE_VEL = 112
+
+
+
+
 
 # Protocol version
 PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
 
 # Default setting
-DXL_IDs                      = np.arange(1,6)                 # Dynamixel IDs
+DXL_IDs                      = np.array([4])#np.arange(1,6)                 # Dynamixel IDs
 BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
 DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
                                                 # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
@@ -114,8 +131,8 @@ def startComms():
         print("Succeeded to open the port")
     else:
         print("Failed to open the port")
-        print("Press any key to terminate...")
-        getch()
+        print("Press enter to terminate...")
+        raw_input()
         quit()
 
     # Set port baudrate
@@ -123,9 +140,19 @@ def startComms():
         print("Succeeded to change the baudrate")
     else:
         print("Failed to change the baudrate")
-        print("Press any key to terminate...")
-        getch()
+        print("Press enter to terminate...")
+        raw_input()
         quit()
+
+def setHomingOffset(id, offset):
+    rad_per_value = 0.088 * (np.pi / 180)
+    offset = offset/rad_per_value
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, id,
+                                                              ADDR_HOMING_OFFSET, offset)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
 
 # Enable Dynamixel Torque
 def enableTorque():
@@ -148,6 +175,7 @@ def disableTorque():
             print("%s" % packetHandler.getRxPacketError(dxl_error))
 
 def readAngles():
+    rad_per_value = 0.088*(np.pi/180)
     q_read = np.zeros([len(DXL_IDs)])
     count = 0
     for id in DXL_IDs:
@@ -159,11 +187,18 @@ def readAngles():
             print("%s" % packetHandler.getRxPacketError(dxl_error))
         q_read[count] = dxl_present_position
         count = count + 1
-    return q_read
-def commandAngles(q_command):
-    if len(DXL_IDs) == len(q_command):
+    return q_read*rad_per_value
+
+def commandAngles(IDs, q_command):
+
+    if q_command is not np.ndarray:
+        q_command = np.array([q_command])
+
+    rad_per_value = 0.088*(np.pi/180)
+    q_command = np.array(q_command/rad_per_value, dtype=np.int)
+    if len(IDs) == len(q_command):
         count = 0
-        for id in DXL_IDs:
+        for id in IDs:
             dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, id, ADDR_PRO_GOAL_POSITION,
                                                                       q_command[count])
             if dxl_comm_result != COMM_SUCCESS:
@@ -174,8 +209,46 @@ def commandAngles(q_command):
     else:
         print("Number of IDs and commanded angles doesn't match")
 
-def setAngleBounds(id, bounds):
-    angles = 0
+def setAngleBounds(id, min_position, max_position):
+    rad_per_value = 0.088 * (np.pi/180)
+    min_position = int(np.rint(min_position/rad_per_value))
+    max_position = int(np.rint(max_position/rad_per_value))
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, id,
+                                                              ADDR_MIN_POSITION_LIMIT,min_position)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, id,
+                                                              ADDR_MAX_POSITION_LIMIT, max_position)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+def setProfileVelocity(id, velocity):
+    #Convert from radians per second to units
+    conv_fact = 1/0.229
+    velocity = velocity*conv_fact
+    velocity = int(velocity)
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, id,
+                                                              ADDR_PROFILE_VEL, velocity)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+def setProfileAcceleration(id, acceleration):
+    #Convert from radians per second^2 to units
+    conv_fact = 1/214.577
+    acceleration = int(acceleration*conv_fact)
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, id,
+                                                              ADDR_PROFILE_ACC, acceleration)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
 
 def endComms():
     portHandler.closePort()
